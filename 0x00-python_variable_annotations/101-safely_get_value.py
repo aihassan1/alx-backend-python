f35a2384d772e584c0b5765e75bbacccefe37808
aihassan1 <abdosruor57@gmail.com>
2024-05-02 17:41:52 +0300

    101

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,9 +9,11 @@ def safely_get_value(
     dct: Mapping, key: Any, default: Union[T, None] = None
 ) -> Union[Any, T]:
     """
-    Safely retrieves the value associated with the given key from the dictionary
+    Safely retrieves the value associated with the given key
+    from the dictionary
     Returns:
-        The value associated with the key if it exists in the dictionary, otherwise the default value.
+        The value associated with the key if it exists in
+        the dictionary, otherwise the default value.
     """
     if key in dct:
         return dct[key]
